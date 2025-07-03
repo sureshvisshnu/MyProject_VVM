d91222f7db2bde3bc1c13e97adaf63892e84219d
sureshvisshnu <SRI MEENAKSHI STORE@DESKTOP-9LVCDHG>
2025-07-03 21:40:24 +0530

    changes made in FormCatalog

--- Fa/views/catalog/FormCatalogBarCodePrint.Designer.cs
+++ Fa/views/catalog/FormCatalogBarCodePrint.Designer.cs
@@ -38,13 +38,13 @@
             BtnPrint = new Button();
             Print = new StatusStrip();
             PrintErrorMsg = new ToolStripStatusLabel();
-            TextBoxStartLocation = new fa.views.controls.text.UserControlPoint();
+            TextBoxStartLocation = new controls.text.UserControlPoint();
             groupBox2 = new GroupBox();
-            ComboBoxDefaultPrinter = new fa.views.controls.ComboBoxSwapTextBox();
+            ComboBoxDefaultPrinter = new controls.ComboBoxSwapTextBox();
             label3 = new Label();
             ComboBoxLabelSize = new ComboBox();
             LabelLabelSize = new Label();
-            YesNoRadioPaperSize = new fa.views.controls.YesNoRadio();
+            YesNoRadioPaperSize = new controls.YesNoRadio();
             Print.SuspendLayout();
             groupBox2.SuspendLayout();
             SuspendLayout();
@@ -59,7 +59,6 @@
             // 
             // AccountIdTransport
             // 
-            AccountIdTransport.Location = new Point(0, 186);
             AccountIdTransport.Size = new Size(116, 21);
             // 
             // checkBoxIsPatient
