10fe87faafc961d8f0e19250d6f52f970d36ac49
sureshvisshnu <SRI MEENAKSHI STORE@DESKTOP-9LVCDHG>
2025-07-05 12:00:52 +0530

    Changes for Percentasge Table Created and CountSalesTax modification done

---
+++ FADataAccessLibrary/Migrations/20250705060840_ProductPercentageTables.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FADataAccessLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductPercentageTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductPercentages",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProductCode = table.Column<string>(type: "varchar(14)", maxLength: 14, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ProductId = table.Column<long>(type: "bigint", nullable: false),
+                    AddedCostPercentage = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    RetailMarginPercentage = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    WholesaleMarginPercentage = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    MrpPercentage = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    IsActive = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Notes = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RowVersion = table.Column<DateTime>(type: "timestamp(6)", rowVersion: true, nullable: true)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn),
+                    CreatedDate = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CompanyId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductPercentages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductPercentages_CatalogItems_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "CatalogItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductPercentages_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPercentage_ProductCode",
+                table: "ProductPercentages",
+                column: "ProductCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPercentages_CompanyId",
+                table: "ProductPercentages",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPercentages_ProductId",
+                table: "ProductPercentages",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductPercentages");
+
+        }
+    }
+}
---
+++ FADataAccessLibrary/Migrations/20250705061947_ForCountrySalesTaxModification.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FADataAccessLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class ForCountrySalesTaxModification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RowVersion",
+                table: "ProductPercentages",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp(6)",
+                oldRowVersion: true,
+                oldNullable: true)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EffectiveTo",
+                table: "CountrySaleTaxs",
+                type: "datetime2(6)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EffectiveFrom",
+                table: "CountrySaleTaxs",
+                type: "datetime2(6)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 5, 11, 49, 44, 137, DateTimeKind.Local).AddTicks(8373));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 5, 11, 49, 44, 137, DateTimeKind.Local).AddTicks(8397));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 5, 11, 49, 44, 137, DateTimeKind.Local).AddTicks(8403));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 5, 11, 49, 44, 137, DateTimeKind.Local).AddTicks(8407));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RowVersion",
+                table: "ProductPercentages",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp(6)",
+                oldRowVersion: true,
+                oldNullable: true)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EffectiveTo",
+                table: "CountrySaleTaxs",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2(6)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EffectiveFrom",
+                table: "CountrySaleTaxs",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2(6)");
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 5, 11, 38, 36, 383, DateTimeKind.Local).AddTicks(5109));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 5, 11, 38, 36, 383, DateTimeKind.Local).AddTicks(5140));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 5, 11, 38, 36, 383, DateTimeKind.Local).AddTicks(5146));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 5, 11, 38, 36, 383, DateTimeKind.Local).AddTicks(5150));
+        }
+    }
+}
