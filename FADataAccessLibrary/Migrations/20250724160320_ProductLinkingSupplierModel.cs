cbd5ae501162343610485b40a7dc94c4ce7a6683
sureshvisshnu <SRI MEENAKSHI STORE@DESKTOP-9LVCDHG>
2025-07-24 21:35:21 +0530

    Table created to link Product with supplier

---
+++ FADataAccessLibrary/Migrations/20250724160320_ProductLinkingSupplierModel.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FADataAccessLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductLinkingSupplierModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RowVersion",
+                table: "ProductPercentages",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp(6)",
+                oldRowVersion: true,
+                oldNullable: true)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.CreateTable(
+                name: "SupplierProducts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    SupplierId = table.Column<long>(type: "bigint", nullable: false),
+                    ProductId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SupplierProducts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SupplierProducts_CatalogItems_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "CatalogItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SupplierProducts_Suppliers_SupplierId",
+                        column: x => x.SupplierId,
+                        principalTable: "Suppliers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 24, 21, 33, 16, 516, DateTimeKind.Local).AddTicks(4144));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 24, 21, 33, 16, 516, DateTimeKind.Local).AddTicks(4174));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 24, 21, 33, 16, 516, DateTimeKind.Local).AddTicks(4179));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 24, 21, 33, 16, 516, DateTimeKind.Local).AddTicks(4183));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SupplierProducts_ProductId",
+                table: "SupplierProducts",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SupplierProducts_SupplierId",
+                table: "SupplierProducts",
+                column: "SupplierId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SupplierProducts");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RowVersion",
+                table: "ProductPercentages",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp(6)",
+                oldRowVersion: true,
+                oldNullable: true)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 21, 21, 14, 13, 216, DateTimeKind.Local).AddTicks(1671));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 21, 21, 14, 13, 216, DateTimeKind.Local).AddTicks(1699));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 21, 21, 14, 13, 216, DateTimeKind.Local).AddTicks(1705));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 21, 21, 14, 13, 216, DateTimeKind.Local).AddTicks(1710));
+        }
+    }
+}
