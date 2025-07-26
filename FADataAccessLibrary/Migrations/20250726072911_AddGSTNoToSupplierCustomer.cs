4a8ca0ad1c40cdb40321051cc509f81277b5125b
sureshvisshnu <SRI MEENAKSHI STORE@DESKTOP-9LVCDHG>
2025-07-26 13:02:29 +0530

    GST No Field added to Supplier Customer

---
+++ FADataAccessLibrary/Migrations/20250726072911_AddGSTNoToSupplierCustomer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FADataAccessLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGSTNoToSupplierCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GSTNo",
+                table: "Suppliers",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RowVersion",
+                table: "ProductPercentages",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp(6)",
+                oldRowVersion: true,
+                oldNullable: true)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GSTNo",
+                table: "Customers",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 26, 12, 59, 7, 356, DateTimeKind.Local).AddTicks(4751));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 26, 12, 59, 7, 356, DateTimeKind.Local).AddTicks(4776));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 26, 12, 59, 7, 356, DateTimeKind.Local).AddTicks(4782));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 26, 12, 59, 7, 356, DateTimeKind.Local).AddTicks(4787));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GSTNo",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "GSTNo",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RowVersion",
+                table: "ProductPercentages",
+                type: "timestamp(6)",
+                rowVersion: true,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp(6)",
+                oldRowVersion: true,
+                oldNullable: true)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 24, 21, 33, 16, 516, DateTimeKind.Local).AddTicks(4144));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 24, 21, 33, 16, 516, DateTimeKind.Local).AddTicks(4174));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 24, 21, 33, 16, 516, DateTimeKind.Local).AddTicks(4179));
+
+            migrationBuilder.UpdateData(
+                table: "CountrySaleTaxs",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "EffectiveTo",
+                value: new DateTime(2400, 7, 24, 21, 33, 16, 516, DateTimeKind.Local).AddTicks(4183));
+        }
+    }
+}
